465341a676550ded679766b4a45054d092d1393b
Tomáš Sušovský <tomas.susovsky@gmail.com>
2015-03-02 21:10:58 +0100

    Updated welcome message :)

--- labyrinth2015-server/server.cpp
+++ labyrinth2015-server/server.cpp
@@ -1,10 +1,21 @@
+/**
+ * @project: LABYRINT 2015
+ * ICP project
+ * @author: xsusov01, xband00
+ * @file: server.c
+ * @date: 2.3.2015
+ * @brief: server side of labyrint application
+*/
+
+/// TDD - test first, enjoyment from pure coding later
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Welcome to Labyrint server" << endl;
     return 0;
 }
 
